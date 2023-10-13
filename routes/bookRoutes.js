afb146f70bde2fb62c7ab19095ad8f27d22d2b17
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2023-10-13 10:19:11 +0530

    updated basic swagger doc for apis

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -9,11 +9,243 @@ const {
 } = require("../controllers/bookControllers");
 const router = express.Router();
 
+/**
+ * @openapi
+ * '/api/books':
+ *  get:
+ *     summary: Fetches all books
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *          application/json:
+ *            schema:
+ *                type: object
+ *                properties:
+ *                  _id: 
+ *                    type: string
+ *                  title: 
+ *                    type: string
+ *                  author:  
+ *                    type: string
+ *                  publicationYear:  
+ *                    type: number
+ *                  isbn: 
+ *                    type: string
+ *                  description: 
+ *                    type: string
+ *                  createdAt: 
+ *                    type: string
+ *                  updatedAt:
+ *                    type: string
+ *                  __v: 
+ *                    type: number
+ */
 router.get("/", retriveAllBooks);
+
+
+/**
+ * @openapi
+ * '/api/books/:id':
+ *  get:
+ *     summary: Fetches specific book ID
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *          application/json:
+ *            schema:
+ *                type: object
+ *                properties:
+ *                  _id: 
+ *                    type: string
+ *                  title: 
+ *                    type: string
+ *                  author:  
+ *                    type: string
+ *                  publicationYear:  
+ *                    type: number
+ *                  isbn: 
+ *                    type: string
+ *                  description: 
+ *                    type: string
+ *                  createdAt: 
+ *                    type: string
+ *                  updatedAt:
+ *                    type: string
+ *                  __v: 
+ *                    type: number
+ */
 router.get("/:id", retriveBookByID);
+
+
+/**
+ * @openapi
+ * '/api/books':
+ *  post:
+ *     summary: Create new book
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - title
+ *              - author
+ *              - publicationYear
+ *              - isbn
+ *              - description
+ *            properties:
+ *              title:  
+ *                 type: string
+ *              author: 
+ *                 type: string
+ *              publicationYear: 
+ *                 type: number
+ *              isbn: 
+ *                 type: string
+ *              description: 
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *          application/json:
+ *            schema:
+ *                type: object
+ *                properties:
+ *                  _id: 
+ *                    type: string
+ *                  title: 
+ *                    type: string
+ *                  author:  
+ *                    type: string
+ *                  publicationYear:  
+ *                    type: number
+ *                  isbn: 
+ *                    type: string
+ *                  description: 
+ *                    type: string
+ *                  createdAt: 
+ *                    type: string
+ *                  updatedAt:
+ *                    type: string
+ *                  __v: 
+ *                    type: number
+ */
+
+
 router.post("/", createBook);
+
+
+
+/**
+ * @openapi
+ * '/api/books/search':
+ *  post:
+ *     summary: Get search results
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - contents
+ *            properties:
+ *              contents: 
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *          application/json:
+ *            schema:
+ *                type: object
+ *                properties:
+ *                  _id: 
+ *                    type: string
+ *                  title: 
+ *                    type: string
+ *                  author:  
+ *                    type: string
+ *                  publicationYear:  
+ *                    type: number
+ *                  isbn: 
+ *                    type: string
+ *                  description: 
+ *                    type: string
+ *                  createdAt: 
+ *                    type: string
+ *                  updatedAt:
+ *                    type: string
+ *                  __v: 
+ *                    type: number
+ */
+
 router.post("/search", searchAPI);
+
+
+/**
+ * @openapi
+ * '/api/books/:id':
+ *  put:
+ *     summary: Updates specific book by ID
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - contents
+ *            properties:
+ *              contents: 
+ *                 type: string
+ * 
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *          application/json:
+ *            schema:
+ *                type: object
+ *                properties:
+ *                  acknowledged: 
+ *                    type: boolean
+ *                  title: 
+ *                    type: string
+ *                  modifiedCount:  
+ *                    type: number
+ *                  upsertedId:  
+ *                    type: string
+ *                  upsertedCount: 
+ *                    type: number
+ *                  matchedCount: 
+ *                    type: number
+ */
+
+
 router.put("/:id", updateBookByID);
+
+/**
+ * @openapi
+ * '/api/books/:id':
+ *  delete:
+ *     summary: Delete specific book by ID
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *          application/json:
+ *            schema:
+ *                type: object
+ *                properties:
+ *                  message: 
+ *                    type: string
+ */
+
 router.delete("/:id", deleteBookByID);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const swaggerUI = require("swagger-ui-express")
+const swaggerJsDoc =  require("swagger-jsdoc")
 const bookRoutes = require("./routes/bookRoutes");
 const connectDb = require("./config/dbConnection");
 const connectElastic = require("./config/elasticConnection");
 
 connectDb();
 connectElastic();
+const options  = {
+  definition:{
+    openapi:'3.0.0',
+    basePath: "/api/books",
+    info:{
+      title:"BOOKS API",
+      version:"1.0.0",
+      description:"Books API"
+    },
+      servers:[
+        {
+          url:process.env.BACKEND_HOSTED_URL
+        }
+      ],
+  },
+  apis:['./routes/*.js']
+}
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const specs = swaggerJsDoc(options)
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/books", bookRoutes);
+app.use("/api-docs",swaggerUI.serve, swaggerUI.setup(specs))
 
 app.get("/", (req, res) => {
   res.status(200).json({ status: "healty" });
